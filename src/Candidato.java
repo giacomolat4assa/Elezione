fbf22302f7d5b5c09173c2437e40210ea9d5f77c
giacomolat4assa <giacomo.latassa06@gmail.com>
2025-06-18 16:27:45 +0200

    Aggiunti attributi e get-set a Candidato.java

--- src/Candidato.java
+++ src/Candidato.java
@@ -1,4 +1,20 @@
 
 public class Candidato {
-
+	private String nome;
+	private int anni;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getAnni() {
+		return anni;
+	}
+	public void setAnni(int anni) {
+		this.anni = anni;
+	}
+	
+	
 }
